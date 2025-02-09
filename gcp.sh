5969ec9a08573c40f514d6620965f5dc2a420fe7
meow <davidjohannesSD@gmail.com>
2025-02-09 03:20:07 +0100

    updated githelper

--- gcp.sh
+++ gcp.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
 
-# get current branch
+export FZF_DEFAULT_OPTS="--bind 'j:down,k:up' --header='=== Maven-Ctrl Menu ===' --multi"
+
 get_current_branch() {
   git branch --show-current
 }
 
-# get remote of branch
+get_remotes() {
+  git remote -v | awk '{print $1}' | uniq
+}
+
+get_files() {
+  git status --porcelain | awk '{print $2}'
+}
+
 get_remote() {
   local branch
   branch=$(git symbolic-ref --short -q HEAD)
   git config --get branch."$branch".remote
 }
 
-# get commit message
+git status
 read -rp "Enter commit message: " commit_message
+selected_files=$(get_files | fzf --prompt='Select files to add: ')
+for file in $selected_files; do
+  git add "$file"
+done
 
-# assigned default remote/branch
 default_remote=$(get_remote)
 default_branch=$(get_current_branch)
+remotes=$(get_remotes)
 
-# if Upstream not set...
 if [ -z "$default_remote" ]; then
-  echo "No upstream remote found. Please enter the remote:"
-  read -rp "Enter remote: " remote
+  if [[ $(echo "$remotes" | wc -l) -gt 1 ]]; then
+    echo "No upstream remote found. Please select a remote:"
+    remote=$(echo "$remotes" | fzf --prompt='Select remote: ')
+  else
+    remote=$remotes
+  fi
 else
-  read -rp "Enter remote (default: $default_remote): " remote
-  remote=${remote:-$default_remote}
+  remote=$default_remote
 fi
-# if branch name not set...
+
 if [ -z "$default_branch" ]; then
   echo "No upstream branch found. Please enter the branch name:"
   read -rp "Enter branch name: " branch
 else
-  read -rp "Enter branch name (default: $default_branch): " branch
-  branch=${branch:-$default_branch}
+  branch=$default_branch
 fi
 
-
-git add .
 git commit -m "$commit_message"
 git push "$remote" "$branch"
 
