cd38d162175d08a03d8cfbc22bcd37be5336a897
meow <davidjohannesSD@gmail.com>
2025-02-16 17:42:38 +0100

    fixed jdtls & cmp buffer duhiqi

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 -- Add your existing key mappings
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>gq', builtin.find_files, {})
-vim.keymap.set('n', '<leader>q', builtin.git_files, {})
+vim.keymap.set('n', '<leader>q', builtin.find_files, {})
+vim.keymap.set('n', '<leader>gq', builtin.git_files, {})
 vim.keymap.set('n', '<leader>hf', function() 
 
   builtin.find_files({cwd = '~/'}); 
--- nvim/lua/meow/lazy.lua
+++ nvim/lua/meow/lazy.lua
@@ -44,56 +44,8 @@ require("lazy").setup({
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
     'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'neoclide/coc.nvim'
+    'saadparwaiz1/cmp_luasnip'
+--    'neoclide/coc.nvim'
 })
-
--- Set up Mason-lspconfig
-require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "ansiblels", "bashls" } -- Ensure ast_grep is installed
-})
-
--- Set up nvim-cmp
-local cmp = require'cmp'
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    -- Accept currently selected item with Enter
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }, {
-    { name = 'buffer' },
-  })
-})
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
--- Lua LSP setup
-require('lspconfig')['lua_ls'].setup {
-  capabilities = capabilities
-}
-
--- Ansible LSP setup
-require('lspconfig')['ansiblels'].setup {
-  capabilities = capabilities,
-  filetypes = { "yaml.ansible" }
-}
-
--- Bash LSP setup
-require('lspconfig')['bashls'].setup {
-  capabilities = capabilities
-}
 
 
